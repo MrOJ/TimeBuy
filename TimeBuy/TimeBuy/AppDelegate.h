50ff2833ecb895991d99a4f21e5087cd05dba8a7
MrOJ <251362783@qq.com>
2015-08-05 19:23:55 +0800

    Version 1.0.0

---
+++ TimeBuy/TimeBuy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TimeBuy
+//
+//  Created by Mr.OJ on 15/8/5.
+//  Copyright (c) 2015年 Mr.OJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
