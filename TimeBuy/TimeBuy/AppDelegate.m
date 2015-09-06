532c6e7c66adcdcacb70797b815a13be052279d0
MrOJ <251362783@qq.com>
2015-09-06 18:44:10 +0800

    update the amap kit

--- TimeBuy/TimeBuy/AppDelegate.m
+++ TimeBuy/TimeBuy/AppDelegate.m
@@ -71,12 +71,11 @@
         NSLog(@"Opened %@", menu);
         
         [[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:UIStatusBarAnimationSlide];
-        //[SlideNavigationController sharedInstance].rightBarButtonItem.width  = 0.01;
         
         shadowView.hidden = NO;
         
         if ([menu isEqualToString:@"left"]) {
-
+            NSLog(@"left");
         }
         
     }];
